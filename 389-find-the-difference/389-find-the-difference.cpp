d7cb8561688f18f1bf5dc93712eba7a1339a4b29
Rohit Multani <rohitmultani153@gmail.com>
2022-08-18 08:14:59 +0530

    Time: 0 ms (100.00%), Space: 6.7 MB (67.70%) - LeetHub

---
+++ 389-find-the-difference/389-find-the-difference.cpp
@@ -0,0 +1,13 @@
+class Solution {
+public:
+    char findTheDifference(string s, string t) {
+        int ans = 0;
+        for(int i=0;i<t.length();i++){
+            ans=ans^t[i];
+        }
+        for(int i=0;i<s.length();i++){
+            ans=ans^s[i];
+        }
+        return ans;
+    }
+};
