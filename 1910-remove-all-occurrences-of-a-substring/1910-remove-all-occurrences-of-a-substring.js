d46279039b68f61c583275166e3299b2a9af0c18
Rohit Multani <rohitmultani153@gmail.com>
2022-08-29 23:55:34 +0530

    Time: 3 ms (92.22%), Space: 6.4 MB (98.00%) - LeetHub

--- 1910-remove-all-occurrences-of-a-substring/1910-remove-all-occurrences-of-a-substring.js
+++ 1910-remove-all-occurrences-of-a-substring/1910-remove-all-occurrences-of-a-substring.js
@@ -1,14 +1,9 @@
-/**
- * @param {string} s
- * @param {string} part
- * @return {string}
- */
-var removeOccurrences = function(s, part) {
-    let lenS=s.length;
-    let lenP=part.length;
-    while(s.includes(part)){
-        let pos = s.indexOf(part);
-        s = s.slice(0,pos) + s.slice(pos+lenP);
+class Solution {
+public:
+    string removeOccurrences(string s, string part) {
+        while(s.length()!=0 && s.find(part)<s.length()){
+            s.erase(s.find(part),part.length());
+        }
+        return s;
     }
-    return s;
 };
