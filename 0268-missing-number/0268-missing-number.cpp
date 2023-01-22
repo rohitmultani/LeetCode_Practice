491cdccaabce6a8fbab76b01545e72fbeb7537c2
Rohit Multani <rohitmultani153@gmail.com>
2023-01-22 18:46:33 +0530

    Time: 26 ms (55.49%), Space: 18 MB (68.14%) - LeetHub

---
+++ 0268-missing-number/0268-missing-number.cpp
@@ -0,0 +1,14 @@
+class Solution {
+public:
+    int missingNumber(vector<int>& nums) {
+        int n=nums.size();
+        int result=0;
+        for(int i=0;i<=n;i++){
+            result=result^i;
+        }
+        for(int i=0;i<n;i++){
+            result=result^nums[i];
+        }
+        return result;
+    }
+};
