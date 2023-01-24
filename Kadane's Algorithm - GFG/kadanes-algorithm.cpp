78e96c3171715ef28944183f8d8ef526a1c6b0b1
Rohit Multani <rohitmultani153@gmail.com>
2023-01-24 23:53:47 +0530

    Added solution - LeetHub

--- Kadane's Algorithm - GFG/kadanes-algorithm.cpp
+++ Kadane's Algorithm - GFG/kadanes-algorithm.cpp
@@ -14,17 +14,17 @@ class Solution{
         long long maxSum=INT_MIN;
         long long currSum=0;
         
-        long long max_so_far = INT_MIN, max_ending_here = 0;
+        // long long maxSum = INT_MIN, max_ending_here = 0;
  
     for (int i = 0; i < n; i++) {
-        max_ending_here = max_ending_here + arr[i];
-        if (max_so_far < max_ending_here)
-            max_so_far = max_ending_here;
+        currSum = currSum + arr[i];
+        if (maxSum < currSum)
+            maxSum = currSum;
  
-        if (max_ending_here < 0)
-            max_ending_here = 0;
+        if (currSum < 0)
+            currSum = 0;
     }
-    return max_so_far;
+    return maxSum;
     }
 };
 
