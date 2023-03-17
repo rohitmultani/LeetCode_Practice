ce5e1dcbc18bb6599afd27568dc2e94856b33e0a
Rohit Multani <rohitmultani153@gmail.com>
2023-03-18 00:14:08 +0530

    Added solution - LeetHub

---
+++ Wildcard string matching - GFG/wildcard-string-matching.cpp
@@ -0,0 +1,59 @@
+//{ Driver Code Starts
+#include<bits/stdc++.h>
+using namespace std;
+
+// } Driver Code Ends
+// User function template for C++
+class Solution{
+    public:
+    bool solve(string &str, string &pattern,int i,int j,vector<vector<int>> &dp){
+        if(i<0 && j<0)
+        return true;
+
+        if(i>=0 && j<0)
+        return false;
+
+        if(i<0 && j>=0){
+            for(int k=0;k<=j;k++){
+               if(pattern[k]!='*')
+               return false;
+           }
+           return true;
+        }
+
+        if(dp[i][j]!=-1)
+        return dp[i][j];
+
+        if(str[i]==pattern[j] || pattern[j]== '?')
+            return dp[i][j]=solve(str,pattern,i-1,j-1,dp);
+        else if(pattern[j]=='*')
+            return dp[i][j]=(solve(str,pattern,i-1,j,dp) || solve(str,pattern,i,j-1,dp));
+        else return dp[i][j] = false;
+    }
+
+    bool match(string p, string s)
+    {
+        vector<vector<int>> dp(s.length(),vector<int>(p.length(),-1));
+        return solve(s,p,s.length()-1,p.length()-1,dp);
+    }
+};
+
+//{ Driver Code Starts.
+int main()
+{
+    int t;
+    cin>>t;
+    while(t--)
+    {
+        string wild, pattern;
+        cin>>wild>>pattern;
+        
+        Solution ob;
+        if(ob.match(wild, pattern))
+        cout<<"Yes\n";
+        else
+        cout<<"No\n";
+    }
+    return 0;
+}
+// } Driver Code Ends
