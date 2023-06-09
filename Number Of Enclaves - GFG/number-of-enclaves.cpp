92b58deb0e92243ab2cc2836cfff675f5a8d88ee
Rohit Multani <rohitmultani153@gmail.com>
2023-06-09 14:54:01 +0530

    Added solution - LeetHub

--- Number Of Enclaves - GFG/number-of-enclaves.cpp
+++ Number Of Enclaves - GFG/number-of-enclaves.cpp
@@ -9,92 +9,54 @@ using namespace std;
 // User function Template for C++
 
 class Solution {
-    private:
-    void dfs(int row, int col, vector<vector<int>> &vis, vector<vector<int>> &mat,int delrow[],int delcol[]){
-    vis[row][col]=1;
-    int n = mat.size();
-    int m = mat[0].size();
-    
-    for(int i=0;i<4;i++){
-        int nrow=row+delrow[i];
-        int ncol=col+delcol[i];
-        
-        if(nrow>=0 && nrow<n && ncol>=0 && ncol<m 
-        && !vis[nrow][ncol] && mat[nrow][ncol]==1){
-            dfs(nrow,ncol,vis,mat,delrow,delcol);
-        }
-    }
-}
   public:
-    int numberOfEnclaves(vector<vector<int>> &mat) {
-        int n = mat.size();
-        int m = mat[0].size();
+    int numberOfEnclaves(vector<vector<int>> &grid) {
+        int n = grid.size();
+        int m = grid[0].size();
         
-    //     vector<vector<int>> vis(n,vector<int> (m,0));
-        
-    //     queue<pair<pair<int,int>,int>> q;
-	    
-	   // for(int i=1;i<n-1;i++){
-	   //     for(int j=1;j<m-1;j++){
-	   //         if(grid[i][j]==1){
-	   //             q.push({{i,j},0});
-	   //             vis[i][j]=1;
-	   //         }
-	   //     }
-	   // }
-	    
-	   // int delrow[]={-1,0,1,0};
-	   // int delcol[]={0,1,0,-1};
-	    
-	   // int maxCnt=0;
-	    
-	   // while(!q.empty()){
-	   //     int row = q.front().first.first;
-	   //     int col = q.front().first.second;
-	   //     int step = q.front().second;
-	   //     q.pop();
-	        
-	   //     maxCnt=max(maxCnt,step);
-	        
-	   //     for(int i=0;i<4;i++){
-	   //         int nrow=row+delrow[i];
-	   //         int ncol=col+delcol[i];
-	   //         if(nrow>=0 && nrow<n && ncol>=0 && ncol<m 
-	   //          && vis[nrow][ncol]==0){
-	   //             vis[nrow][ncol]=1;
-	   //             q.push({{nrow,ncol},step+1});
-	   //         }
-	   //     }
-	       
-	       
-	   // }
-	    vector<vector<int>> vis(n,vector<int>(m,0));
-        int delrow[]={-1,0,1,0};
-        int delcol[]={0,1,0,-1};
-        
-        for(int j=0;j<m;j++){
-            if(!vis[0][j] && mat[0][j]==1){
-                dfs(0,j,vis,mat,delrow,delcol);
-            }
-            if(!vis[n-1][j] && mat[n-1][j]==1){
-                dfs(n-1,j,vis,mat,delrow,delcol);
-            }
-        }
-        
-        for(int i=0;i<n;i++){
-            if(!vis[i][0] && mat[i][0]==1){
-                dfs(i,0,vis,mat,delrow,delcol);
-            }
-            if(!vis[i][m-1] && mat[i][m-1]==1){
-                dfs(i,m-1,vis,mat,delrow,delcol);
-            }
-        }
-        int cnt = 0;
+        int vis[n][m]={0};
+        queue<pair<int,int>> q;
         for(int i=0;i<n;i++){
             for(int j=0;j<m;j++){
-                if(!vis[i][j] && mat[i][j]==1){
-                    cnt++;
+                if(i==0 || i==n-1 || j==0 || j==m-1){
+                    if(grid[i][j]==1)
+                    {
+                        q.push({i,j});
+                        vis[i][j]=1;
+                    }
                 }
+            }
+        }
+        
+        int delrow[] = {-1,0,1,0};
+        int delcol[] = {0,1,0,-1};
+        
+        
+        while(!q.empty()){
+            int row = q.front().first;
+            int col = q.front().second;
+            
+            q.pop();
+            
+            for(int i=0;i<4;i++){
+                int nrow = row + delrow[i];
+                int ncol =  col + delcol[i];
+                
+                if(nrow>=0 && nrow<n && ncol>=0 && ncol<m
+                && vis[nrow][ncol]==0 && grid[nrow][ncol]==1){
+                    q.push({nrow,ncol});
+                    vis[nrow][ncol]=1;
+                }
+            }
+        }
+        
+        int cnt=0;
+        
+        for(int i=0;i<n;i++){
+            for(int j=0;j<m;j++){
+                
+                if(grid[i][j]==1 && vis[i][j]==0)
+                cnt++;
             }
         }
         
