1d8e36d981aac8c7e6762e71947f623c370af75f
Rohit Multani <rohitmultani153@gmail.com>
2023-06-12 09:11:39 +0530

    Added solution - LeetHub

--- Eventual Safe States - GFG/eventual-safe-states.cpp
+++ Eventual Safe States - GFG/eventual-safe-states.cpp
@@ -9,43 +9,43 @@ using namespace std;
 // User function Template for C++
 
 class Solution {
-    private:
-    bool dfs(int node,vector<int> adj[],int vis[],int pathVis[],int check[]){
-        vis[node]=1;
-        pathVis[node]=1;
-        check[node]=0;
-        for(auto it:adj[node]){
-            if(!vis[it]){
-                if(dfs(it,adj,vis,pathVis,check)==true){
-                    check[node]=0;
-                    return true;
-                }
-                
-            }
-            else if(pathVis[it])
-            return true;
-        }
-        check[node]=1;
-        pathVis[node]=0;
-        return false;
-    }
   public:
     vector<int> eventualSafeNodes(int V, vector<int> adj[]) {
-        int vis[V]={0};
-        int pathVis[V]={0};
-        vector<int> ans;
-        int check[V]={0};
+        vector<int> adjRev[V];
+        int indegree[V]={0};
+        
         for(int i=0;i<V;i++){
-            if(!vis[i]){
-                dfs(i,adj,vis,pathVis,check);
+            //i->it;
+            // rev it->i;
+            for(auto it:adj[i]){
+                adjRev[it].push_back(i); // 
+                indegree[i]++;
             }
         }
         
+        queue<int> q;
+        vector<int> ans;
         for(int i=0;i<V;i++){
-            if(check[i]==1)
-            ans.push_back(i);
+            if(indegree[i]==0){
+                q.push(i);
+            }
         }
         
+        
+        while(!q.empty()){
+            int node = q.front();
+            q.pop();
+            ans.push_back(node);
+            for(auto it:adjRev[node]){
+                indegree[it]--;
+                if(indegree[it]==0){
+                    q.push(it);
+                }
+            }
+        }
+        
+        
+        sort(ans.begin(),ans.end());
         return ans;
     }
 };
